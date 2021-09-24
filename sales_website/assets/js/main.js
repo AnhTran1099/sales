4b4c15a28379a9aee33ff8e41e1e288b17ece9df
AnhTran <anh94701@gmail.com>
2021-09-24 09:21:03 +0700

    change

--- sales_website/assets/js/main.js
+++ sales_website/assets/js/main.js
@@ -1,5 +1,16 @@
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if ($(this).scrollTop()) {
+            $('#goto-top').fadeIn();
+        } else {
+            $('#goto-top').fadeOut();
+        }
+    });
+    $("#goto-top").click(function () {
+        $("html, body").animate({scrollTop: 0}, 1000);
+    });
+});
 
-
-setTimeout(function() {
-    alert('Chào mừng bạn đến với TA Shop!!!')
-}, 1000)
+// setTimeout(function() {
+//     alert('Chào mừng bạn đến với TA Shop!!!')
+// }, 1000)
